28e19025bcd4fe96b4c3303cb1f40ba99abfa6c9
paranoidkid <uryaya2010@gmail.com>
2026-07-05 19:14:57 +0300

    basic movement &  punch

--- street_fighter.py
+++ street_fighter.py
@@ -19,6 +19,13 @@ class Fighter:
         self.is_jumping = False
         self.is_crouching = False
 
+        # combat related
+        self.is_attacking = False
+        self.attack_timer = 0
+        self.attack_rect = None
+        self.facing_right = True
+        self.has_hit = False
+
     def jump(self, moving_left: bool, moving_right: bool):
         if not self.is_jumping:
             self.vel_y = self.jump_power
@@ -49,7 +56,21 @@ class Fighter:
         if not self.is_jumping and not self.is_crouching:
             self.rect.x -= self.speed
     
-    def update(self, screen_height: int, screen_width: int):
+    def attack(self, target):
+        if not self.is_attacking:
+            self.is_attacking = True
+            self.attack_timer = 10   # attack hitbox stays for 10 frames
+
+            self.has_hit = False
+
+            self.facing_right = self.rect.centerx < target.rect.centerx
+
+            if self.facing_right:
+                self.attack_rect = pygame.Rect(self.rect.right, self.rect.y + 20, 50, 20)
+            else:
+                self.attack_rect = pygame.Rect(self.rect.left - 50, self.rect.y + 20, 50, 20)
+
+    def update(self, screen_height: int, screen_width: int, target):
         
         self.vel_y += self.gravity
         self.rect.y += self.vel_y
@@ -70,8 +91,31 @@ class Fighter:
         if self.rect.right > screen_width:
             self.rect.right = screen_width
 
+        if self.is_attacking:
+            self.attack_timer -= 1
+
+            if self.attack_rect != None:
+                if self.facing_right:
+                    self.attack_rect.left = self.rect.right
+                else:
+                    self.attack_rect.right = self.rect.left
+
+                self.attack_rect.y = self.rect.y + 20
+
+                if self.attack_rect.colliderect(target.rect) and not self.has_hit:
+                    target.health -= 10
+                    self.has_hit = True
+                    print(f"target health is now {target.health}")
+
+            if self.attack_timer == 0:
+                self.is_attacking = False
+                self.attack_rect = None
+
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
+
+        if self.is_attacking and self.attack_rect:
+            pygame.draw.rect(screen, (0, 0, 0), self.attack_rect)
 
 pygame.init()
 
@@ -88,16 +132,23 @@ screen_width = screen.get_width()
 
 clock = pygame.Clock()
 
-WHITE = (255, 255, 255)
+game_font = pygame.font.SysFont(None, 36)
 
-player1 = Fighter(350, screen_height - 100, (0, 0, 255)) # player 1 = blue
-player2 = Fighter(900, screen_height - 100, (255, 0, 0)) # player 2 = red
+player1 = Fighter(screen_width / 2 - screen_width / 4, screen_height - 100, (0, 0, 255)) # player 1 = blue
+player2 = Fighter(screen_width / 2 + screen_width / 4, screen_height - 100, (255, 0, 0)) # player 2 = red
+
+player1_win_msg = game_font.render("PLAYER ONE WINS!", True, (0, 0, 255))
+player2_win_msg = game_font.render("PLAYER TWO WINS!", True, (255, 0, 0))
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_u:
+                player1.attack(player2)
 
     keys = pygame.key.get_pressed()
 
@@ -121,13 +172,34 @@ while running:
     if keys[pygame.K_ESCAPE]:
         running = False
     
-    screen.fill(WHITE)
+    screen.fill((255, 255, 255)) # white
 
-    player1.update(screen_height, screen_width)
-    player2.update(screen_height, screen_width)
+    player1.update(screen_height, screen_width, player2)
+    player2.update(screen_height, screen_width, player1)
 
-    player1.draw(screen)
-    player2.draw(screen)
+    player1_health_bar = pygame.Rect(50, 50, player1.health * 2, 50)
+    player2_health_bar = pygame.Rect(screen_width - 50 - player2.health * 2, 50, player2.health * 2, 50)
+
+    pygame.draw.rect(screen, (255, 0, 0), player1_health_bar)
+    pygame.draw.rect(screen, (255, 0, 0), player2_health_bar)
+
+    if player2.health <= 0:
+        player1.draw(screen)
+        screen.blit(player1_win_msg, (screen_width / 2 - player1_win_msg.get_width() / 2, screen_height / 2 - player1_win_msg.get_height() / 2))
+        pygame.display.flip()
+        pygame.time.wait(2000)
+        running = False
+        continue
+    elif player1.health <= 0:
+        player2.draw(screen)
+        screen.blit(player2_win_msg, (screen_width / 2 - player2_win_msg.get_width() / 2, screen_height / 2 - player2_win_msg.get_height() / 2))
+        pygame.display.flip()
+        pygame.time.wait(2000)
+        running = False
+        continue
+    else:
+        player1.draw(screen)
+        player2.draw(screen)
 
     pygame.display.flip()
 
